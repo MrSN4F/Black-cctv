adc1fe3f6999e57272cfb96d0be2cf4ac8fc2717
MrSN4F <87918075+MrSN4F@users.noreply.github.com>
2021-07-30 18:31:34 +0700

    Add files via upload

---
+++ cctv.sh
@@ -0,0 +1,50 @@
+#!/bin/sh/hack-cctv
+# tampilan
+toilet HACK CCTV | lolcat
+echo "=====================================" | lolcat
+echo "(+)AUTHOR  : MR•SN4F              (+)" | lolcat
+echo "(+)TEAM    : INDONESIAN DEFACER   (+)" | lolcat
+echo "(+)youtube : BLACK AR             (+)" | lolcat
+echo "=====================================" | lolcat
+echo
+echo " [1]. Mulai Hack" | lolcat
+echo "-------------------------------------" | lolcat
+echo " [2]. Keluar / Exit Dari Sini" | lolcat
+echo "-------------------------------------" | lolcat
+echo
+read -p "Pilih  => " gady
+clear
+sleep 1
+    echo "SUBSCRIBE DULU TOD.." | lolcat
+       sleep 3
+           xdg-open https://youtube.com/channel/UCdmeveqsu1QZns11mT054Ig
+clear
+
+sleep 1
+# data masukan
+if [ $gady = 1 ]; then
+echo "Sedang Menginstal"
+git clone https://github.com/MrVirusSpm-07/mobile
+sleep 2
+echo "Sebentar Lagi"
+cd mobile
+echo "Okee Selesai"
+sleep 1
+clear
+
+# tampilan 2
+figlet HACK-CCTV | lolcat
+echo "#############################################" | lolcat
+echo "(+) AUTHOR : MR•SN4F                      (+)" | lolcat
+echo "(+) TEAM : INDONESIAN DEFACER             (+)" | lolcat
+echo "(+) youtube : BLACK AR                    (+)" | lolcat
+echo "#############################################" | lolcat
+sleep 1
+sh https_mrvirus_mobile.sh
+fi
+
+# data ke 2
+if [ $gady = 2 ]; then
+echo "BABAY SAYANG.." | lolcat
+exit
+fi
